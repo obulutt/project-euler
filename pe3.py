87a9b8c6d4263c954403bdde01a7530661b97f10
obulutt <98688107+obulutt@users.noreply.github.com>
2024-12-20 00:11:28 +0300

    pe3.py

---
+++ pe3.py
@@ -0,0 +1,10 @@
+def largest_prime_factor(n):
+    factor = 2
+    while factor * factor <= n:
+        if n % factor == 0:
+            n //= factor
+        else:
+            factor += 1
+    return n
+number = 600851475143
+print(largest_prime_factor(number))
